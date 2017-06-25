a7074b724ba698db4356b0404f8b0a716f50c25c
Abdiel Nie <1051957834@qq.com>
2017-06-25 14:02:06 +0800

    Add files via upload

---
+++ include/syscalls.h
@@ -0,0 +1,15 @@
+/*
+ *Copyright 2013-2014 by Explorer Developer.
+ *made by 迷彩红星<1@GhostBirdOS.org>
+ *Explorer 系统调用头
+ *ExplorerOS/Kernel/include/syscalls.h
+ *version:Alpha
+ *8/11/2014 12:06 AM
+ */
+
+#ifndef SYSCALLS_H_
+#define SYSCALLS_H_
+
+void do_sys_calls(void);/*不能直接调用这个函数*/
+
+#endif
