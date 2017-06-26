a816441195454368613669ff82b4900ad4ba8208
Abdiel Nie <1051957834@qq.com>
2017-06-26 20:01:22 +0800

    Add files via upload

---
+++ include/process.h
@@ -0,0 +1,58 @@
+/*
+ *Copyright 2013-2014 by Explorer Developer.
+ *made by 迷彩红星<1@GhostBirdOS.org>
+ *Explorer 进程管理代码头文件
+ *ExplorerOS/Kernel/include/peocess.h
+ *version:Alpha
+ *7/29/2014 4:59 PM
+ */
+
+#ifndef PROCESS_H_
+#define PROCESS_H_
+
+#include "types.h"
+#include "address.h"
+
+#define THREAD_SIZE		8192
+
+union thread_union kernel_thread;
+union thread_union *thread_union_point;
+u32 creat_thread_id;
+u32 creat_process_id;
+
+typedef u32 pid_t;
+
+struct thread_info
+{
+	u32 count;
+	u32 esp0;
+	union thread_union *next_thread_union;
+	u32 *cr3;
+	u32 pid;
+	u32 ppid;
+	u32 default_count;
+	u32 thread_id;
+	u8 filename[32];
+};
+
+union thread_union
+{
+	struct thread_info thread_info;
+	unsigned long stack[THREAD_SIZE / sizeof(long)];
+};
+
+struct header
+{
+	char flag[4];
+	void *entry;
+};
+
+void init_process(void);
+void Creat_process(u8 *filename);
+
+u32 get_tid(void);
+u32 get_ppid(void);
+u32 get_pid(void);
+
+
+#endif
