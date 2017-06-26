c4ae71527331f3d88a9828f96f59956b193c93ae
Abdiel Nie <1051957834@qq.com>
2017-06-26 21:33:24 +0800

    Add files via upload

---
+++ Process/do_creat_thread.asm
@@ -0,0 +1,19 @@
+﻿;全局函数
+global	do_creat_thread
+
+
+
+;代码区
+[section .text]
+[bits 32]
+;Warning:能自由使用的寄存器只有EAX\ECX\EDX
+;do_creat_thread(void *point)
+;未完成
+do_creat_thread:
+	;创建一个新PCB(eax=新进程PCB首地址)
+	;call	creat_empty_process
+	;数据指针指向PCB
+	;mov		ecx,[thread_union_point]
+	;获取cr3
+	mov		edx,[esi+12]
+	ret
---
+++ Process/restart.asm
@@ -0,0 +1,25 @@
+﻿;全局变量
+extern	code_3_selector
+extern	data_3_selector
+;全局函数
+global	restart
+
+;代码区
+[section .text]
+[bits 32]
+;Warning:能自由使用的寄存器只有EAX\ECX\EDX
+restart:
+	mov		edx,esp
+	mov		ecx,[data_3_selector]
+	or		ecx,3
+	push	ecx
+	push	edx
+	pushfd
+	mov		eax,[code_3_selector]
+	or		eax,3
+	push	eax
+	push	.next
+	iretd
+.next:
+	jmp		$
+	
