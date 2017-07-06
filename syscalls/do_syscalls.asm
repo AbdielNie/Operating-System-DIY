24866acfb243dc0f821232748907c6fb0d772a57
Abdiel Nie <1051957834@qq.com>
2017-07-06 20:59:00 +0800

    Update do_syscalls.asm

--- syscalls/do_syscalls.asm
+++ syscalls/do_syscalls.asm
@@ -7,9 +7,6 @@ global	do_sys_calls
 [section .text]
 [bits 32]
 
-;do_sys_calls函数由迷彩红星<1@GhostBirdOS.org>制作
-;更好的实现方式应当是根据eax的功能号从功能表中跳入相应的函数位置执行
-;为了方便实行，目前仅仅判断中断号执行（这个方法不适合大量调用）
 
 do_sys_calls:
 	cmp		eax,0x1
