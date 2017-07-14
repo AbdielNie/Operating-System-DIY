04fa6f95e2bc1c50bf9663dbcdfa21aafb39cb62
聂良玉 <1051957834@qq.com>
2017-07-15 00:00:13 +0800

    Update stdlib.h

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,12 +1,3 @@
-/*
- *Copyright 2013-2014 by Explorer Developer.
- *made by √‘≤ ∫Ï–«<1@GhostBirdOS.org>
- *stddef.h
- *ExplorerOS/Kernel/include/stdlib.h
- *version:Alpha
- *8/18/2014 10:23 AM
- */
-
 #ifndef STDLIB_H_
 #define STDLIB_H_
 
