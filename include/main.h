c78fdc485462edb57a912799ac14098ed923623e
聂良玉 <1051957834@qq.com>
2017-07-14 23:58:05 +0800

    Update main.h

--- include/main.h
+++ include/main.h
@@ -1,11 +1,3 @@
-/*
- *Copyright 2013-2014 by 2013-2014 by Explorer OS Developer. All rights reserved.
- *made by √‘≤ ∫Ï–«<1@GhostBirdOS.org>
- *Explorer 0.01 main.cÕ∑
- *Explorer/Kernel/include/main.h
- *7/24/2014 10:56 AM
- */
-
 #ifndef MAIN_H_
 #define MAIN_H_
 
