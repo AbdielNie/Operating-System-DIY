05c419abf79b81ceb33917fbf665a23354ba0395
聂良玉 <1051957834@qq.com>
2017-07-14 23:59:47 +0800

    Update stddef.h

--- include/stddef.h
+++ include/stddef.h
@@ -1,12 +1,3 @@
-/*
- *Copyright 2013-2014 by Explorer Developer.
- *made by ÃÔ²ÊºìÐÇ<1@GhostBirdOS.org>
- *Explorer C±ê×¼stddef.h
- *ExplorerOS/Kernel/include/stddef.h
- *version:Alpha
- *8/7/2014 9:36 AM
- */
-
 #ifndef STDDEF_H_
 #define STDDEF_H_
 
